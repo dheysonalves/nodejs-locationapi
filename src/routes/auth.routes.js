07d482171d61067fec02d40d42b8a0806a79b28e
Dheyson Alves <dheyson.lucas@hotmail.com>
2021-04-11 13:13:32 -0300

    feat: add error handling for the signin route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -6,17 +6,40 @@ const User = mongoose.model('User');
 const router = express.Router();
 
 router.post('/signup', async (request, response) => {
-	const { email, password } = request.body;
-
 	try {
+		const { email, password } = request.body;
+
 		const user = new User({ email, password });
 		await user.save();
 
-		const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY' );
+		const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
 
 		response.send({ token });
 	} catch (error) {
 		return response.status(422).send(error.message);
+	}
+
+});
+
+router.post('/signin', async (request, response) => {
+	const { email, password } = request.body;
+
+	if (!email || !password) {
+		return response.status(422).send({ error: 'You must provide email and password ' });
+	}
+
+	const user = await User.findOne({ email });
+
+	if (!user) {
+		return response.status(422).send({ error: 'Invalid password or email' });
+	}
+
+	try {
+		await user.comparePassword(password);
+		const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
+		response.send({ token });
+	} catch (error) {
+		return response.status(422).send({ error: 'Invalid password or email' });
 	}
 
 });
