cb4d98393fa5918c48dbd5c0ccdc2f4745169ba2
Dheyson Alves <dheyson.lucas@hotmail.com>
2021-04-26 21:46:09 -0300

    feat: add new routes information

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -8,14 +8,14 @@ const routes = express.Router();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', requireAuth, (request, response) => {
-	const { email } = request.user;
-	response.send(`Your email: ${email} `);
-});
-
+app.use(express.json());
 // For a procedural point of view, we must first bodyParser the data, then made the request.
 app.use(authRoutes);
 app.use(trackRoutes);
 
+app.get('/', requireAuth, (request, response) => {
+	const { email } = request.user;
+	response.send(`Your email: ${email} `);
+});
 
 module.exports = routes;
