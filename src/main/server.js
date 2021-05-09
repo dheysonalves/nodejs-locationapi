693e7249195d159ce3783b2541755d081728ddb4
Dheyson Alves <dheyson.lucas@hotmail.com>
2021-05-09 15:17:03 -0300

    fix: update files from server with main folder

--- src/main/server.js
+++ src/main/server.js
@@ -1,7 +1,19 @@
 require('dotenv').config();
+require('../components/users/User');
+require('../components/tracks/Track');
+require('./config/database.config');
+
+const requireAuth = require('./middlewares/requireAuth');
 
 const app = require('./app');
 
-app.listen(process.env.SERVER_PORT, () => {
+const PORT = process.env.PORT || 3000;
+
+app.get('/', requireAuth, (request, response) => {
+	const { email } = request.user;
+	response.send(`Your email: ${email} `);
+});
+
+app.listen(PORT, () => {
 	console.log(`Server started on port ${process.env.SERVER_PORT}`);
 });
